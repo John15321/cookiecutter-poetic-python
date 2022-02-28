15ce1a44b9a6f64aa28377d17b5301968e970878
John15321 <janbronicki@gmail.com>
2022-02-28 15:12:29 +0100

    generating sphinx proj still not working

--- hooks/post_gen_hooks.py
+++ hooks/post_gen_hooks.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
 
-os.system('poetry add --dev sphinx="*" sphinx-rtd-theme isort tox black pylint flake8="*" pytest toml')
+os.system('echo "chuj" > chuj.txt')
+
+#  ; cd {{ cookiecutter.project_slug }} ; poetry shell ; poetry add --dev sphinx="*" sphinx-rtd-theme isort tox black pylint flake8="*" pytest toml
