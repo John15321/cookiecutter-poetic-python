13fe5684744e0fc8901186650f7de26970106ab0
John15321 <janbronicki@gmail.com>
2022-02-28 14:39:11 +0100

    .

--- hooks/post_gen_hooks.py
+++ hooks/post_gen_hooks.py
@@ -1,1 +1,4 @@
 # -*- coding: utf-8 -*-
+import os
+
+os.system('poetry add --dev sphinx="*" sphinx-rtd-theme isort tox black pylint flake8="*" pytest toml')
--- {{ cookiecutter.project_short_name }}/tests/test_{{ cookiecutter.project_slug }}.py
+++ {{ cookiecutter.project_short_name }}/tests/test_{{ cookiecutter.project_slug }}.py
@@ -1,1 +1,23 @@
 # -*- coding: utf-8 -*-
+import os
+from pathlib import Path
+
+import toml
+
+from {{ cookiecutter.project_slug }} import __version__
+
+def get_version() -> str:
+    """Get version of the package from the ``pyproject.toml`` file.
+
+    Returns:
+        Pcakge version.
+    """
+    Path(os.path.dirname(__file__))
+    root_project_directory = "../"
+
+    pyproject_file: Path = root_project_directory + "pyproject.toml"
+
+    return str(toml.load(pyproject_file)["tool"]["poetry"]["version"])
+
+def test_version():
+    assert __version__ == get_version()
